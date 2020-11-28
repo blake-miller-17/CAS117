d6e2f3ff0ae6090f24950d0aa336fa14711f6504
blake-miller-17 <bgarrettm@gmaail.com>
2020-11-27 19:49:07 -0500

    Adding CAS projects

---
+++ RollerMadness/Assets/Scripts/Treasure.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Treasure : MonoBehaviour {
+
+    [Tooltip("How much the treasure is worth")]
+	public int value = 10;
+    [Tooltip("The game object to spawn when this treasure is collected")]
+	public GameObject explosionPrefab;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			if (GameManagerRollerMadness.gm!=null)
+			{
+                // tell the game manager to Collect
+                GameManagerRollerMadness.gm.Collect (value);
+			}
+			
+			// explode if specified
+			if (explosionPrefab != null) {
+				Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+			}
+			
+			// destroy after collection
+			Destroy (gameObject);
+		}
+	}
+}
